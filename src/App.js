5b61ae6bf98e83427195eb448691e5255d00bce8
nasimqureshi <qureshi.nasim@yahoo.com>
2024-06-18 14:38:19 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Contact from './Contact';
+import './App.css';
+import Footer from './Footer';
+import Header from './Header';
+import Home from './Home';
+import ProductDetail from './ProductDetail';
+import ProductList from './ProductList';
+
+function App() {
+  return (
+    <div className="App">
+     < Contact />
+     <Footer />
+     <Header />
+     <Home />
+     <ProductDetail />
+     <ProductList />
+      
+    </div>
+  );
+}
+
+export default App;
